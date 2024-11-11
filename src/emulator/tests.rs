8793d7624049c211dfea0bf9da31277258dc6628
Rohan Simon <rohan.simon@ufl.edu>
2024-11-11 13:25:28 -0500

    added tests up to SLLI

--- src/emulator/tests.rs
+++ src/emulator/tests.rs
@@ -115,10 +115,24 @@ fn test_JAL_neg_offset() {
     populate(
         &mut instruction_map,
         &[
-            ISA::ADDI.build(Operands {rd: 5, rs1: 0, imm: 0, ..Default::default()}), // ADDI ( x5 := x0 + 1)
-            ISA::ADDI.build(Operands {rd: 5, rs1: 0, imm: 0, ..Default::default()}), // ADDI ( x5 := x0 + 1)
-            ISA::JAL.build(Operands {rd: 1, imm: -4, ..Default::default()}) // JAL (pc = pc - 4)
-        ]        
+            ISA::ADDI.build(Operands {
+                rd: 5,
+                rs1: 0,
+                imm: 0,
+                ..Default::default()
+            }), // ADDI ( x5 := x0 + 1)
+            ISA::ADDI.build(Operands {
+                rd: 5,
+                rs1: 0,
+                imm: 0,
+                ..Default::default()
+            }), // ADDI ( x5 := x0 + 1)
+            ISA::JAL.build(Operands {
+                rd: 1,
+                imm: -4,
+                ..Default::default()
+            }), // JAL (pc = pc - 4)
+        ],
     );
 
     // Instruction fetch
@@ -146,9 +160,11 @@ fn test_JAL_panic() {
     // JAL ( x1 := PC + 4, jump to PC + 0x122)
     populate(
         &mut instruction_map,
-        &[
-            ISA::JAL.build(Operands {rd: 1, imm: 0x122, ..Default::default()})
-        ]
+        &[ISA::JAL.build(Operands {
+            rd: 1,
+            imm: 0x122,
+            ..Default::default()
+        })],
     );
 
     // Instruction fetch
@@ -201,10 +217,25 @@ fn test_JALR_neg_offset() {
     populate(
         &mut instruction_map,
         &[
-            ISA::ADDI.build(Operands {rd: 2, rs1: 0, imm: 1, ..Default::default()}), // ADDI ( x5 := x0 + 1)
-            ISA::ADDI.build(Operands {rd: 2, rs1: 0, imm: 1, ..Default::default()}), // ADDI ( x5 := x0 + 1)
-            ISA::JALR.build(Operands {rd: 1, rs1: 2, imm: -4, ..Default::default()})  // JALR ( x1 := PC + 4, jump to (x2 - 4) & ~1)
-        ]        
+            ISA::ADDI.build(Operands {
+                rd: 2,
+                rs1: 0,
+                imm: 1,
+                ..Default::default()
+            }), // ADDI ( x5 := x0 + 1)
+            ISA::ADDI.build(Operands {
+                rd: 2,
+                rs1: 0,
+                imm: 1,
+                ..Default::default()
+            }), // ADDI ( x5 := x0 + 1)
+            ISA::JALR.build(Operands {
+                rd: 1,
+                rs1: 2,
+                imm: -4,
+                ..Default::default()
+            }), // JALR ( x1 := PC + 4, jump to (x2 - 4) & ~1)
+        ],
     );
 
     // Instruction fetch
@@ -217,7 +248,7 @@ fn test_JALR_neg_offset() {
     // After JALR, x1 should contain PC + 4, and the PC should jump to PC - 4 + 2
     let pc = emulator.state().pipeline.datapath.instr_addr_o;
     emulator.clock(&instruction_map, &mut data_map);
-    assert_eq!(emulator.state().x[1], pc  + 4);
+    assert_eq!(emulator.state().x[1], pc + 4);
     assert_eq!(
         emulator.state().pipeline.datapath.instr_addr_o,
         (pc + emulator.state().x[2] - 4) & !1
@@ -561,7 +592,305 @@ fn test_SLTI() {
     let mut instruction_map: BTreeMap<u32, u8> = BTreeMap::new();
     let mut data_map: BTreeMap<u32, u8> = BTreeMap::new();
 
-    // SLTI ( x1 := x0 + 1)
-    // SLTI ( x1 := x1 + (-1))
-    // SLTI ( x0 := x0 + 1 )
+    // SLTI ( x1 := x0 < 1)
+    // SLTI ( x1 := x1 < (-1))
+    // SLTI ( x0 := x0 < 1 )
+
+    populate(
+        &mut instruction_map,
+        &[
+            ISA::SLTI.build(Operands {
+                rd: 1,
+                rs1: 0,
+                imm: 1,
+                ..Default::default()
+            }),
+            ISA::SLTI.build(Operands {
+                rd: 1,
+                rs1: 1,
+                imm: -1,
+                ..Default::default()
+            }),
+            ISA::SLTI.build(Operands {
+                rd: 0,
+                rs1: 0,
+                imm: 1,
+                ..Default::default()
+            }),
+        ],
+    );
+
+    // Instruction fetch
+    emulator.clock(&instruction_map, &mut data_map);
+
+    // SLTI ( x1 := x0 < 1)
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 1);
+    // SLTI ( x1 := x1 < (-1))
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 0);
+    // SLTI ( x0 := x0 < 1 ) <= Should not change x0
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[0], 0);
+}
+
+#[test]
+fn test_SLTIU() {
+    let mut emulator: Emulator = Emulator::new();
+
+    let mut instruction_map: BTreeMap<u32, u8> = BTreeMap::new();
+    let mut data_map: BTreeMap<u32, u8> = BTreeMap::new();
+
+    // SLTIU ( x1 := x0 < 1)
+    // SLTIU ( x1 := x1 < (-1))
+    // SLTIU ( x0 := x0 < 1 )
+
+    populate(
+        &mut instruction_map,
+        &[
+            ISA::SLTIU.build(Operands {
+                rd: 1,
+                rs1: 0,
+                imm: 1,
+                ..Default::default()
+            }),
+            ISA::SLTIU.build(Operands {
+                rd: 1,
+                rs1: 1,
+                imm: -1, // Should be treated as an unsigned number (pretty large)
+                ..Default::default()
+            }),
+            ISA::SLTIU.build(Operands {
+                rd: 0,
+                rs1: 0,
+                imm: 1,
+                ..Default::default()
+            }),
+        ],
+    );
+
+    // Instruction fetch
+    emulator.clock(&instruction_map, &mut data_map);
+
+    // SLTI ( x1 := x0 < 1)
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 1);
+    // SLTI ( x1 := x1 < (-1))
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 1);
+    // SLTI ( x0 := x0 < 1 ) <= Should not change x0
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[0], 0);
+}
+
+#[test]
+fn test_XORI() {
+    let mut emulator: Emulator = Emulator::new();
+
+    let mut instruction_map: BTreeMap<u32, u8> = BTreeMap::new();
+    let mut data_map: BTreeMap<u32, u8> = BTreeMap::new();
+
+    // XORI ( x1 := x0 ^ 4)
+    // XORI ( x1 := x1 ^ (-1))
+    // XORI ( x0 := x0 ^ 100 )
+
+    populate(
+        &mut instruction_map,
+        &[
+            ISA::XORI.build(Operands {
+                rd: 1,
+                rs1: 0,
+                imm: 4,
+                ..Default::default()
+            }),
+            ISA::XORI.build(Operands {
+                rd: 1,
+                rs1: 1,
+                imm: -1,
+                ..Default::default()
+            }),
+            ISA::XORI.build(Operands {
+                rd: 0,
+                rs1: 0,
+                imm: 100,
+                ..Default::default()
+            }),
+        ],
+    );
+
+    // Instruction fetch
+    emulator.clock(&instruction_map, &mut data_map);
+
+    // XORI ( x1 := x0 ^ 4)
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 4);
+    // XORI ( x1 := x1 ^ (-1))
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1] as i32, -5);
+    // XORI ( x0 := x0 ^ 100 ) <= Should not change x0
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[0], 0);
+}
+
+#[test]
+fn test_ORI() {
+    let mut emulator: Emulator = Emulator::new();
+
+    let mut instruction_map: BTreeMap<u32, u8> = BTreeMap::new();
+    let mut data_map: BTreeMap<u32, u8> = BTreeMap::new();
+
+    // ORI ( x1 := x0 | 12)
+    // ORI ( x1 := x1 | (-1))
+    // ORI ( x0 := x0 | 100 )
+
+    populate(
+        &mut instruction_map,
+        &[
+            ISA::ORI.build(Operands {
+                rd: 1,
+                rs1: 0,
+                imm: 12,
+                ..Default::default()
+            }),
+            ISA::ORI.build(Operands {
+                rd: 1,
+                rs1: 1,
+                imm: -10,
+                ..Default::default()
+            }),
+            ISA::ORI.build(Operands {
+                rd: 0,
+                rs1: 0,
+                imm: 100,
+                ..Default::default()
+            }),
+        ],
+    );
+
+    // Instruction fetch
+    emulator.clock(&instruction_map, &mut data_map);
+
+    // ORI ( x1 := x0 | 12)
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 12);
+    // ORI ( x1 := x1 ^ (-10))
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1] as i32, -2);
+    // ORI ( x0 := x0 ^ 100 ) <= Should not change x0
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[0], 0);
+}
+
+#[test]
+fn test_ANDI() {
+    let mut emulator: Emulator = Emulator::new();
+
+    let mut instruction_map: BTreeMap<u32, u8> = BTreeMap::new();
+    let mut data_map: BTreeMap<u32, u8> = BTreeMap::new();
+
+    populate(
+        &mut instruction_map,
+        &[
+            ISA::ADDI.build(Operands {
+                rd: 1,
+                rs1: 0,
+                imm: 37,
+                ..Default::default()
+            }),
+            ISA::ANDI.build(Operands {
+                rd: 1,
+                rs1: 1,
+                imm: 5,
+                ..Default::default()
+            }),
+            ISA::ANDI.build(Operands {
+                rd: 1,
+                rs1: 1,
+                imm: -10,
+                ..Default::default()
+            }),
+            ISA::ANDI.build(Operands {
+                rd: 0,
+                rs1: 0,
+                imm: 100,
+                ..Default::default()
+            }),
+        ],
+    );
+
+    // Instruction fetch
+    emulator.clock(&instruction_map, &mut data_map);
+
+    // Set x1 := 37
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 37);
+
+    // ANDI ( x1 := x1 & 5)
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 5);
+
+    // ANDI ( x1 := x1 & (-10))
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 4);
+
+    // ANDI ( x0 := x0 & 100 ) <= Should not change x0
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[0], 0);
+}
+
+#[test]
+fn test_SLLI() {
+    let mut emulator: Emulator = Emulator::new();
+
+    let mut instruction_map: BTreeMap<u32, u8> = BTreeMap::new();
+    let mut data_map: BTreeMap<u32, u8> = BTreeMap::new();
+
+    populate(
+        &mut instruction_map,
+        &[
+            ISA::ADDI.build(Operands {
+                rd: 1,
+                rs1: 0,
+                imm: 10,
+                ..Default::default()
+            }),
+            ISA::SLLI.build(Operands {
+                rd: 2,
+                rs1: 1,
+                imm: 4,
+                ..Default::default()
+            }),
+            ISA::SLLI.build(Operands {
+                rd: 3,
+                rs1: 1,
+                imm: 0b100001,
+                ..Default::default()
+            }),
+            ISA::SLLI.build(Operands {
+                rd: 0,
+                rs1: 0,
+                imm: 3,
+                ..Default::default()
+            }),
+        ],
+    );
+
+    // Instruction fetch
+    emulator.clock(&instruction_map, &mut data_map);
+
+    // Set x1 := 10
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[1], 10);
+
+    // SLLI ( x2 := x1 << 4)
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[2], 160);
+
+    // SLLI ( x3 := x1 << 0b1000001) Should only shift 1 time since we only look at last 5 bits
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[3], 20);
+
+    // SLLI ( x0 := x1 << 3 ) <= Should not change x0
+    emulator.clock(&instruction_map, &mut data_map);
+    assert_eq!(emulator.state().x[0], 0);
 }
