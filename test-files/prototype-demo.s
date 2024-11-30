b1f34ef642e2bab8a9d46989e31d16afece87e42
Fiona Cahalan <fionamcahalan@gmail.com>
2024-11-30 15:58:17 -0500

    demo program updates

--- test-files/prototype-demo.s
+++ test-files/prototype-demo.s
@@ -7,104 +7,27 @@ array: .ascii "test"
 .text
 main:
     # Example I-type instruction example
-    ADDI x2, x1, 5      #   x2 = x1 + 5
+    ADDI x2, x9, 5      #   x2 = x9 + 5
     XORI x3, x2, 0xFF   #   x3 = x2 ^ 0xFF
-    SLLI x1, x2, 2      #   x1 = x2 >> 2
+    SLLI x9, x2, 2      #   x9 = x2 >> 2
 
     # U-type instruction example
     LUI x4, 0xFFF       # (load upper immediate) x4 = 0xFFF << 20
 
     # R-type instruction example
-    SUB x6, x3, x2      # x7 = x4 - x2
+    SUB x8, x3, x2      # x8 = x3 - x2
 
     # J-type instruction
-    JAL x5, function    # jump to function, storing return address in x5
-    SW x5, 4(x1)
-    XOR x1, x1, x1
-    XOR x1, x1, x1
-    XOR x1, x1, x1
+    JAL x1, function    # jump to function, storing return address in x1
+    XOR x3, x3, x3      # will not be executed
+    SW x1, 4(x9)        # store value in x1 to address x9 + 4 
+loop_forever:
+    BNE x3, x2, loop_forever
 
 branch_target:
-    ADDI x2, x6, 10
-    JALR x5, x5, 0x00 
+    ADDI x2, x6, 10     # x2 = x6 + 10
+    JALR x1, x1, 0x04   # jump to address at x1 + 4
 
 function:
     # B-type instruction
-    BNE x3, x2, branch_target
-
-
-.data
-message: .string "test\n"
-numbers: .word 1, 2, 3, 4
-bytes: .byte 0xFF, 0x42, 0x33
-array: .ascii "test"
-
-.text
-main:
-    # Example I-type instruction example
-    ADDI x2, x1, 5      #   x2 = x1 + 5
-    XORI x3, x2, 0xFF   #   x3 = x2 ^ 0xFF
-    SLLI x1, x2, 2      #   x1 = x2 >> 2
-
-    # U-type instruction example
-    LUI x4, 0xFFF       # (load upper immediate) x4 = 0xFFF << 20
-
-    # R-type instruction example
-    SUB x6, x3, x2      # x7 = x4 - x2
-
-    # J-type instruction
-    BNE x3, x2, branch_target
-    XOR x5, x5, x5
-    XOR x5, x5, x5
-    # JAL x5, function    # jump to function, storing return address in x5
-
-branch_target:
-    ADDI x2, x6, 10
-    JALR x5, x5, 0x00 
-
-function:
-    # B-type instruction
-    BNE x3, x2, branch_target
-
-
-.data
-message: .string "test\n"
-numbers: .word 1, 2, 3, 4
-bytes: .byte 0xFF, 0x42, 0x33
-array: .ascii "test"
-
-.text
-main:
-    # Example I-type instruction example
-    ADDI x2, x1, 5      #   x2 = x1 + 5
-    XORI x3, x2, 0xFF   #   x3 = x2 ^ 0xFF
-    SLLI x1, x2, 2      #   x1 = x2 >> 2
-
-    # U-type instruction example
-    LUI x4, 0xFFF       # (load upper immediate) x4 = 0xFFF << 20
-
-    # R-type instruction example
-    SUB x6, x3, x2      # x7 = x4 - x2
-
-    # J-type instruction
-    JAL x5, function    # jump to function, storing return address in x5
-    XOR x5, x5, x5
-    XOR x1, x1, x1
-    XOR x1, x1, x1
-    XOR x1, x1, x1
-    XOR x1, x1, x1
-    XOR x1, x1, x1
-
-function:
-    # B-type instruction
-    BNE x3, x2, branch_target
-    XOR x1, x1, x1
-    XOR x1, x1, x1
-    #XOR x6, x5, x5 #THIS EXECUTES
-    # CAN'T DO BACKWARDS BRANCHING
-    # HAVING TO SUBTRACT 8
-
-branch_target:
-    ADDI x2, x6, 10
-    JALR x1, x5, 0x00 
-    XOR x1, x1, x1
+    BNE x3, x2, branch_target   # jumps to branch target
